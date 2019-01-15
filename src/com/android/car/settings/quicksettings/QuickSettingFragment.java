dd03c509ffd27c9bb662778138ba589e3fcddf94
Wang, ArvinX <arvinx.wang@intel.com>
2019-01-15 17:18:51 +0800

    Fix disallow user switch for CTS Device owner Test
    if devices are managed by admin, the current user doesn't allow
    to manage the user.
    
    bug: 120237874
    Test: Launch CTS-VERIFIER, follow the instruction to start the
          Device owner Test ->Disallow user switch
    
    Change-Id: I0cf8d1e9f701cc49ff7e400e325294480803422d
    Signed-off-by: Wang, ArvinX <arvinx.wang@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- src/com/android/car/settings/quicksettings/QuickSettingFragment.java
+++ src/com/android/car/settings/quicksettings/QuickSettingFragment.java
@@ -19,6 +19,7 @@ import android.car.drivingstate.CarUxRestrictions;
 import android.car.user.CarUserManagerHelper;
 import android.content.pm.UserInfo;
 import android.os.Bundle;
+import android.os.UserManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -95,6 +96,14 @@ public class QuickSettingFragment extends BaseFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        final boolean isDemo = UserManager.isDeviceInDemoMode(getContext());
+        mUserSwitcherBtn.setVisibility(showUserSwitcher(isDemo) ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    @Override
     public void onStop() {
         super.onStop();
         mGridAdapter.stop();
@@ -108,6 +117,17 @@ public class QuickSettingFragment extends BaseFragment {
 
         TextView userSwitcherText = (TextView) getActivity().findViewById(R.id.user_switcher_text);
         userSwitcherText.setText(currentUserInfo.name);
+    }
+
+    private boolean showUserSwitcher(boolean isDemo) {
+        if (isDemo || !UserManager.supportsMultipleUsers()) {
+            return false;
+        }
+        UserManager userManager = UserManager.get(getContext());
+        if (userManager.hasUserRestriction(UserManager.DISALLOW_USER_SWITCH)) {
+            return false;
+        }
+        return true;
     }
 
     /**
