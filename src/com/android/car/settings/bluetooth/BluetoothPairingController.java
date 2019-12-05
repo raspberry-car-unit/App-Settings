6dc39d0aa7ba6c3608de1f8bee3697b1e20b6604
Rahul Sabnis <rahulsabnis@google.com>
2019-12-05 10:52:19 -0800

    Remove calls to removed bluetooth apis
    Bug: 143245666
    Test: Manual
    Change-Id: I1dc97ac75346ecd22240cc03d777d2c628cfdb5b

--- src/com/android/car/settings/bluetooth/BluetoothPairingController.java
+++ src/com/android/car/settings/bluetooth/BluetoothPairingController.java
@@ -376,7 +376,6 @@ public class BluetoothPairingController implements OnCheckedChangeListener,
 
             case BluetoothDevice.PAIRING_VARIANT_PASSKEY:
                 int pass = Integer.parseInt(passkey);
-                mDevice.setPasskey(pass);
                 break;
 
             case BluetoothDevice.PAIRING_VARIANT_PASSKEY_CONFIRMATION:
@@ -386,11 +385,8 @@ public class BluetoothPairingController implements OnCheckedChangeListener,
 
             case BluetoothDevice.PAIRING_VARIANT_DISPLAY_PASSKEY:
             case BluetoothDevice.PAIRING_VARIANT_DISPLAY_PIN:
-                // Do nothing.
-                break;
-
             case BluetoothDevice.PAIRING_VARIANT_OOB_CONSENT:
-                mDevice.setRemoteOutOfBandData();
+                // Do nothing.
                 break;
 
             default:
