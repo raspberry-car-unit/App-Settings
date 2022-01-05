1632c713b473657d5d7ffeb5d4090db3c23f59e8
Yinglei Wang <yingleiw@google.com>
2022-01-05 22:41:29 +0000

    Revert "eSIM deprecated API Test Refactoring- Car Settings"
    Revert "UiccPortInfo and SimSlotMapping CTS tests"
    
    Revert "eSIM deprecated API Refactoring"
    
    Revert "SIM deprecated API Refactoring"
    
    Revert "eSIM deprecated API test Refactoring"
    
    Revert submission 1933236-eSIM MEP Refectoring
    
    Reason for revert: DroidMonitor: Potential culprit for Bug X - verifying through Forrest before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted
    Reverted Changes:
    Iedeb90f18:eSIM deprecated API test Refactoring
    I71e4e1ef6:[MEP] eSIM API refactor to support MEP
    I0eca2130f:eSIM deprecated API Refactoring
    Ic3244333b:[MEP] eSIM deprecated API Refactoring
    I121751fbb:eSIM deprecated API Test Refactoring- Car Settings...
    I7356183f9:SIM deprecated API Refactoring
    I5c01626b4:UiccPortInfo and SimSlotMapping CTS tests
    
    Change-Id: I6a746de108a0171059d3603648a062e964a96108

--- tests/robotests/src/com/android/car/settings/network/SubscriptionUtilsTest.java
+++ tests/robotests/src/com/android/car/settings/network/SubscriptionUtilsTest.java
@@ -24,7 +24,6 @@ import static org.mockito.Mockito.when;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
-import android.telephony.UiccPortInfo;
 import android.telephony.UiccSlotInfo;
 
 import com.google.android.collect.Lists;
@@ -33,7 +32,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import java.util.Collections;
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
@@ -107,11 +105,7 @@ public class SubscriptionUtilsTest {
 
     private UiccSlotInfo createUuicSlotInfo(boolean isActive, boolean isEsim, int simSlotIndex,
             String cardId, int cardStateInfo) {
-        return new UiccSlotInfo(
-                isEsim, cardId, cardStateInfo,
-                /* isExtendedApduSupported= */ false, /* isRemovable= */ true,
-                Collections.singletonList(
-                        new UiccPortInfo("" /* iccId */, 0 /* portIdx */,
-                                simSlotIndex /* logicalSlotIdx */, isActive /* isActive */)));
+        return new UiccSlotInfo(isActive, isEsim, cardId, cardStateInfo, simSlotIndex,
+                /* isExtendedApduSupported= */ false, /* isRemoveable= */ true);
     }
 }
