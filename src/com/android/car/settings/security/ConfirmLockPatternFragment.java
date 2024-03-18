85a2858b3d61d3e500caf289156294c6f12e088c
Ellen Arteca <emarteca@google.com>
2024-03-18 22:08:26 +0000

    Ensure that LockscreenCredential#close() gets called
    Wrap LockscreenCredential objects in try-with-resources.
    This ensures the LockscreenCredential's `close()` method gets
    called, which clears the `byte[]` field that stores the LSKF.
    
    Bug: 320392352
    Test: presubmit
    
    Change-Id: If180967bcfc1d812edd4202b2d049a4b69d6105f

--- src/com/android/car/settings/security/ConfirmLockPatternFragment.java
+++ src/com/android/car/settings/security/ConfirmLockPatternFragment.java
@@ -179,7 +179,15 @@ public class ConfirmLockPatternFragment extends BaseFragment {
 
     private void onCheckCompleted(boolean lockMatched, int timeoutMs) {
         if (lockMatched) {
-            mCheckLockListener.onLockVerified(LockscreenCredential.createPattern(mPattern));
+            LockscreenCredential patternCred = LockscreenCredential.createPattern(mPattern);
+            mCheckLockListener.onLockVerified(patternCred);
+            // onLockVerified does not take ownership of the LockscreenCredential if it is
+            // a CheckLockListenerActivity see CheckLockActivity#onLockVerified
+            // however, if the listener is a VerifyLockChangeActivity then it _does_ take
+            // ownership, so we cannot wrap the LockscreenCredential in a try-with-resources
+            if (mCheckLockListener instanceof CheckLockActivity) {
+                patternCred.close();
+            }
         } else {
             if (timeoutMs > 0) {
                 mConfirmLockLockoutHelper.onCheckCompletedWithTimeout(timeoutMs);
@@ -189,7 +197,8 @@ public class ConfirmLockPatternFragment extends BaseFragment {
 
                 // Set timer to clear wrong pattern
                 mLockPatternView.removeCallbacks(mClearErrorRunnable);
-                mLockPatternView.postDelayed(mClearErrorRunnable, CLEAR_WRONG_ATTEMPT_TIMEOUT_MS);
+                mLockPatternView.postDelayed(mClearErrorRunnable,
+                    CLEAR_WRONG_ATTEMPT_TIMEOUT_MS);
             }
         }
     }
