f4e9383a6fd849eedc1e32af04aff25fb0529097
Praveen Kumar G S <praveenkumar.gs@amd.com>
2024-08-01 18:14:36 +0530

    Developer option crashed when WIFI feature disabled
    When WIFI feature is disabled, crash observed with developer option
    is selected
    
    Bug: 349269872
    Change-Id: I016457a01a0ad8c76a97de0445ee184aa04b2ced

--- src/com/android/car/settings/wifi/CarWifiManager.java
+++ src/com/android/car/settings/wifi/CarWifiManager.java
@@ -82,9 +82,11 @@ public class CarWifiManager implements WifiPickerTracker.WifiPickerTrackerCallba
                 + "{" + Integer.toHexString(System.identityHashCode(this)) + "}",
                 android.os.Process.THREAD_PRIORITY_BACKGROUND);
         mWorkerThread.start();
-        mWifiTracker = WifiUtil.createWifiPickerTracker(lifecycle, context,
-                new Handler(Looper.getMainLooper()), mWorkerThread.getThreadHandler(),
-                /* listener= */ this);
+        if (mWifiManager != null) {
+            mWifiTracker = WifiUtil.createWifiPickerTracker(lifecycle, context,
+                    new Handler(Looper.getMainLooper()), mWorkerThread.getThreadHandler(),
+                    /* listener= */ this);
+        }
     }
 
     /**
@@ -266,9 +268,11 @@ public class CarWifiManager implements WifiPickerTracker.WifiPickerTrackerCallba
 
     @Override
     public void onWifiStateChanged() {
-        int state = mWifiTracker.getWifiState();
-        for (Listener listener : mListeners) {
-            listener.onWifiStateChanged(state);
+        if (mWifiTracker != null) {
+            int state = mWifiTracker.getWifiState();
+            for (Listener listener : mListeners) {
+                listener.onWifiStateChanged(state);
+            }
         }
     }
 }
