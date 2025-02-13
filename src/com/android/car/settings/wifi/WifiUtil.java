b4e49446c0ad0928a67555efefe66de871c5a249
jinbao liu <quic_jinbliu@quicinc.com>
2025-02-13 18:17:10 +0800

    wifi(CarSettings): Add metered/privacy UI option
    Add metered/privacy(randomized mac) option for wifi network.
    1. secured network before connection
    2. connected network
    3. hidden network
    
    Change-Id: I1e793ae8cb08a9830f94936b59b5683e60f3e3c1
    (cherry picked from commit 1163842b65ca8188543dadd8a759b7656af04636)

--- src/com/android/car/settings/wifi/WifiUtil.java
+++ src/com/android/car/settings/wifi/WifiUtil.java
@@ -229,24 +229,26 @@ public class WifiUtil {
      * @param listener for callbacks on success or failure of connection attempt (can be null)
      */
     public static void connectToWifiEntry(Context context, String ssid, int security,
-            String password, boolean hidden, @Nullable WifiManager.ActionListener listener) {
+            String password, boolean hidden, int metered, int privacy,
+            @Nullable WifiManager.ActionListener listener) {
         WifiManager wifiManager = context.getSystemService(WifiManager.class);
-        WifiConfiguration wifiConfig = getWifiConfig(ssid, security, password, hidden);
+        WifiConfiguration wifiConfig = getWifiConfig(ssid, security, password,
+                                                     hidden, metered, privacy);
         wifiManager.connect(wifiConfig, listener);
     }
 
     private static WifiConfiguration getWifiConfig(String ssid, int security,
-            String password, boolean hidden) {
+            String password, boolean hidden, int metered, int privacy) {
         WifiConfiguration wifiConfig = new WifiConfiguration();
         wifiConfig.SSID = String.format("\"%s\"", ssid);
         wifiConfig.hiddenSSID = hidden;
 
-        return finishWifiConfig(wifiConfig, security, password);
+        return finishWifiConfig(wifiConfig, security, password, metered, privacy);
     }
 
     /** Similar to above, but uses WifiEntry to get additional relevant information. */
     public static WifiConfiguration getWifiConfig(@NonNull WifiEntry wifiEntry,
-            String password) {
+            String password, int metered, int privacy) {
         WifiConfiguration wifiConfig = new WifiConfiguration();
         if (wifiEntry.getWifiConfiguration() == null) {
             wifiConfig.SSID = "\"" + wifiEntry.getSsid() + "\"";
@@ -255,11 +257,12 @@ public class WifiUtil {
             wifiConfig.hiddenSSID = wifiEntry.getWifiConfiguration().hiddenSSID;
         }
 
-        return finishWifiConfig(wifiConfig, wifiEntry.getSecurity(), password);
+        return finishWifiConfig(wifiConfig, wifiEntry.getSecurity(), password,
+                                metered, privacy);
     }
 
     private static WifiConfiguration finishWifiConfig(WifiConfiguration wifiConfig, int security,
-            String password) {
+            String password, int metered, int privacy) {
         switch (security) {
             case WifiEntry.SECURITY_NONE:
                 wifiConfig.setSecurityParams(WifiConfiguration.SECURITY_TYPE_OPEN);
@@ -311,6 +314,9 @@ public class WifiUtil {
             default:
                 throw new IllegalArgumentException("unknown security type " + security);
         }
+        wifiConfig.meteredOverride = metered;
+        wifiConfig.macRandomizationSetting = privacy == WifiEntry.PRIVACY_RANDOMIZED_MAC
+                ? WifiConfiguration.RANDOMIZATION_AUTO : WifiConfiguration.RANDOMIZATION_NONE;
         return wifiConfig;
     }
 
